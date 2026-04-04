7e0776c7ba3f459a153263fd51f1f1868d90e2de
surogharagyozyan03-cyber <suro.gharagyozyan03@gmail.com>
2026-04-05 01:59:40 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,1 @@
-const Database = require('better-sqlite3');
 
-const db = new Database('./data/sfpd.db');
-
-db.prepare(`
-  UPDATE users 
-  SET role='admin', approved=1, can_use_prefix=1 
-  WHERE nickname='Mayk Fyurze'
-`).run();
-
-const u = db.prepare(`
-  SELECT nickname, role, approved 
-  FROM users 
-  WHERE nickname='Mayk Fyurze'
-`).get();
-
-console.log(u);
